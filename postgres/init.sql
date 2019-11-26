49405382166c20f27a373416d26d31fdfebd7042
Vahur Varris <varrisvahur@gmail.com>
2019-11-26 17:26:35 +0100

    Changed init script

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,6 +1,5 @@
 CREATE TABLE prefix_temp(
     prefix_cidr cidr NOT NULL,
-    prefix_timestamp TIMESTAMP NOT NULL,
-    as_path TEXT NOT NULL,
-    unique(prefix_cidr, prefix_timestamp, as_path)
+    prefix_timestamp INTEGER NOT NULL,
+    as_path TEXT NOT NULL
 );
