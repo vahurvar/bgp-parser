e3031ec0c9f73726d3084f8d9c4b85b9ed8e17a4
Vahur Varris <varrisvahur@gmail.com>
2019-11-27 21:48:59 +0100

    Updated insert script

--- parse-scripts/insert.sh
+++ parse-scripts/insert.sh
@@ -1,6 +1,6 @@
 for var in "$@"
 do
     echo "$var"
-    psql -U root bgp -c "COPY prefix_temp(prefix_timestamp, prefix_cidr, as_path) FROM '/$var' WITH DELIMITER ',' CSV;"
+    PGPASSWORD= psql -h 127.0.0.1 -p 80 -U root bgp -c "\copy prefix_temp(prefix_timestamp, prefix_cidr, as_path) FROM '/home/varrisvahur/$var' WITH DELIMITER ',' CSV;"
     rm $var
 done
