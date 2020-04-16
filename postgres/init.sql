28f283b858abc36899513a437cbd49ee98da33fe
Vahur Varris <varrisvahur@gmail.com>
2020-04-16 18:49:59 +0200

    Fixed mistake in init script

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,20 +1,13 @@
 create table prefix_temp (
 	prefix_cidr cidr,
-	prefix_timestamp integer[],
+	prefix_timestamp integer,
 	as_path text
 );
 
 create table prefix_data (
 	prefix_cidr cidr,
-	prefix_timestamp integer,
+	prefix_timestamp integer[],
 	as_path text
-);
-
-create table raw_data
-(
-	prefix_cidr cidr not null,
-	prefix_timestamp integer[] not null,
-	as_path text not null
 );
 
 create table prefix_cidr
